34af37915f9bf2dba2a5582b6f360c8d907e7c43
henryvalle <57874459+henryvalle@users.noreply.github.com>
2020-11-04 14:59:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World-Happy Learning. This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
+ <?php echo '<p>Hola muundo- Sigo Aprendiendo! Este es mi primer pagina con PHP200 usando Jenkins y OpenShift Online</p>'; ?> 
  </body>
 </html>
